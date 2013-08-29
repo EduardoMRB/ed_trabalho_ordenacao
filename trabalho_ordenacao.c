4f3ec06580695aeed0c37fa80fe4f3abba985738
Eduardo Borges <eduardomrb@gmail.com>
2013-08-28 20:01:21 -0400

    commit inicial

---
+++ trabalho_ordenacao.c
@@ -0,0 +1,26 @@
+#include <stdlio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * Autores: Eduardo Borges, Michel Rodrigues e Jean Osowski
+ * 
+ * Eduardo Borges   - Método de ordenação: Radix Sort
+ *
+ * Michel Rodrigues - Método de ordenação: Bubble Sort
+ *
+ * Jean Osowski     - Método de ordenação: Quick Sort
+ *
+ */
+
+typedef struct mercadoria {
+    int id;
+    float valor;
+    char *nome;
+} Mercadoria;
+
+
+
+int main() {
+
+}
